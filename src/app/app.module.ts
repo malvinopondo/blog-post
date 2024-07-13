382e91280cd0597de5ce0c158605ab68f016d159
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-07-13 11:40:00 +0300

    feat(Routes): Set routing for my app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
-import { CategoryAvbarComponent } from './layout/category-avbar/category-avbar.component';
+import { CategoryAvbarComponent } from './layout/category-avbar/category-navbar.component';
 import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SingleCategoryComponent } from './pages/single-category/single-category.component';
