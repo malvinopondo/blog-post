e554da2ca53e1e3f996899ffab0c6cd0df57547f
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-07-13 11:03:58 +0300

    feat(navbar): Create a navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
+import { CategoryAvbarComponent } from './layout/category-avbar/category-avbar.component';
+import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CategoryAvbarComponent,
+    CategoryNavbarComponent
   ],
   imports: [
     BrowserModule,
